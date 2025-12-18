c748bb360136db655653e97e7049f05ffcc7ed2f
Matthew Teets <matthewteets@pingidentity.com>
2025-12-18 11:24:15 -0800

    Added DV config props

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@ export const daVinciConfig = {
   companyId: "484c8d69-2783-4b55-b787-be71c4cd1532",
 
   // Your DaVinci API Key from the DaVinci application
-  apiKey: "YOUR_DAVINCI_API_KEY",
+  apiKey: "9e606ef2506c0686c59dd7c03a1dc405595c2c99d8c3a5fcd9445590c71815eb297b10b28238876329b206cd1323e36acb8a81bbf0f553f9e5ac05ea8a4a30db8a6f6a07ac3e95de55db94ee9ef58935e7ead1b754dea2fd2968bd9efee7bb73e48a4ec1697bd2fbe34269c99a7676c2f1885c5a8c3702493ce37c5f5794e26a",
 
   // Your DaVinci flow policy ID
-  policyId: "YOUR_POLICY_ID",
+  policyId: "a2fb6e133e976381ec1bfc4e7cd36ae8",
 
   // PingOne region: 'com' (North America), 'eu' (Europe), 'asia' (Asia Pacific), 'ca' (Canada)
   region: "com",
